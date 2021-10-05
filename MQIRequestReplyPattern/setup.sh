3ef5b3855f0a49ab30f733d4e4a5826d77457c59
Brian S Paskin <21156194+bpaskin@users.noreply.github.com>
2021-10-05 05:04:01 -0400

    corrected paths

--- MQIRequestReplyPattern/setup.sh
+++ MQIRequestReplyPattern/setup.sh
@@ -1,6 +1,6 @@
 /opt/mqm/bin/crtmqm QM1HACK
 /opt/mqm/bin/crtmqm QM2HACK
-strmqm QM1HACK
-strmqm QM2HACK
-runmqsc QM1HACK < QM1HACK.mqsc
-runmqsc QM2HACK < QM2HACK.mqsc
+/opt/mqm/bin/strmqm QM1HACK
+/opt/mqm/bin/strmqm QM2HACK
+/opt/mqm/bin/runmqsc QM1HACK < QM1HACK.mqsc
+/opt/mqm/bin/runmqsc QM2HACK < QM2HACK.mqsc
